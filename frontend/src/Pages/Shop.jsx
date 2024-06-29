2e021c9f6aa0c94e7aa117da303c69b2ea5b5fc6
alexToMars <javierreynosoqxk12@gmail.com>
2024-06-29 00:37:01 -0600

    Feature: Item ppopular products without stiling

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/Popular/Popular'
+/*Checar min 50 */
 const Shop = () => {
   return (
-    <div>Shop</div>
+    <div>
+      <Hero/>
+      <Popular />
+    </div>
   )
 }
 
