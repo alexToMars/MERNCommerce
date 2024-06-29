3552e66917e25e8714ec286407366647b8c09417
alexToMars <javierreynosoqxk12@gmail.com>
2024-06-29 01:11:24 -0600

    Feature : Offers apartado

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
-/*Checar min 50 */
+import Offers from '../Components/Offers/Offers'
+
 const Shop = () => {
   return (
     <div>
       <Hero/>
       <Popular />
+      <Offers />
     </div>
   )
 }
