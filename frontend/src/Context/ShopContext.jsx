fcac606920b474ad8bc4a0b5cde7e6c953cc0b03
alexToMars <javierreynosoqxk12@gmail.com>
2024-07-01 15:17:20 -0600

    Adding ShopContext function that affecta navbar and cartitems

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext ,useState, useEffect } from "react";
+import React, { createContext ,useState } from "react";
 import all_product from "../Components/Assets/all_product"
 
 export const ShopContext = createContext(null);
@@ -23,11 +23,28 @@ const ShopContextProvider = (props)=>{
         setCartItems((prev) =>({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    useEffect(() => {
-        console.log(cartItems); // Imprime el estado actualizado
-    }, [cartItems]);
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for ( const item in cartItems){
+            if (cartItems[item] > 0){
+                let itemInfo = all_product.find((product) =>product.id === Number(item))
+                totalAmount += itemInfo.new_price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    }
 
-    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+    const getTotalCartItems = () =>{
+        let totalItem = 0;
+        for (const item in cartItems){
+            if (cartItems[item] > 0){
+                totalItem += cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {all_product,cartItems,getTotalCartItems,getTotalCartAmount,addToCart,removeFromCart};
     return (
         <ShopContext.Provider value ={contextValue}>
             {props.children}
