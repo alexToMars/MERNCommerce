e96dd0947e8dd310656605d1c1733c9ac1079cda
alexToMars <javierreynosoqxk12@gmail.com>
2024-07-01 02:35:42 -0600

    feature:Adding ProductDisplay

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ import whatsapp_icon from '../Assets/whatsapp_icon.png'
 const Footer = () => {
   return (
     <div className='footer'>
-        <div className="footer logo">
+        <div className="footer-logo">
             <img src={footer_logo} alt="" />
             <p>SHOPPER</p>
         </div>
