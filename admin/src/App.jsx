f9787ea5ab3617d598aee477affd495f7f33c845
alexToMars <javierreynosoqxk12@gmail.com>
2024-07-05 04:13:17 -0600

    Feature: Adding navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
---
+++ admin/src/Pages/Admin/Admin.jsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Admin.css'
+
+const Admin = () => {
+  return (
+    <div className='admin'></div>
+  )
+}
+
+export default Admin
